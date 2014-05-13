a1380b8fea982819a368af5bf2d82a621babb83d
Your Name <bkveton@bkveton.local>
2014-05-13 00:33:03 -0500

    Restructure site, choose fonts, add minimal styling and color

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$("document").ready(function() {
 
+  var navHeight = $("#navbar").outerHeight();
+
+  var menuHeight = $("#menu").outerHeight();
+
+  var show = $("section.show");
+
+  var bio = $("section.bio");
+
+  $("#content").css("margin-top", navHeight).css("margin-bottom", menuHeight);
+
+/*  show.click(function() {
+    $(this).children().children("section.bio").addClass("open");
+  });
+
+  bio.click(function() {
+    $(this).removeClass("open");
+  });
+
+*/
+
+});
