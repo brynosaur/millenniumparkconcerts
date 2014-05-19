11cb0eeeaaff88513a27c73de7c4b89ca71253fd
brynosaur <bkveton2@gmail.com>
2014-05-19 14:23:23 -0500

    Center home page vertical

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ $("document").ready(function() {
 
   var menuHeight = $("#menu").outerHeight();
 
+  var windowHeight = $(window).outerHeight();
+
   var show = $("section.show");
 
   var bio = $("section.bio");
 
   $("#content").css("padding-top", navHeight).css("padding-bottom", menuHeight);
+
+  $(".banner").css("height", windowHeight);
 
 /*  show.click(function() {
     $(this).children().children("section.bio").addClass("open");
